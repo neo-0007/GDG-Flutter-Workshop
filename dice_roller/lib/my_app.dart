1087dde04f36cc9cfeeeea1a161361eac7480a1a
neo-0007 <hrishikeshgohain123@gmail.com>
2024-12-25 19:11:20 +0530

    feat:add a appbar to main page

--- dice_roller/lib/my_app.dart
+++ dice_roller/lib/my_app.dart
@@ -4,15 +4,24 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 26, 2, 80),
-        body: DiceRollerPage()
-      ),
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 19, 105, 255),
+            title: const Text(
+              'Dice Roller',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 26, 2, 80),
+          body: DiceRollerPage()),
     );
   }
 }
